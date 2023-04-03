6e1c354b93c21ed867f89be9b5f6e874d788c3f6
ostapix@gmail.com <@Bandera345>
2023-04-03 15:12:19 +0300

    Add Tag list, create,update and delete views

--- todo_list/views.py
+++ todo_list/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from todo_list.models import Task
+from todo_list.forms import TaskForm
+from todo_list.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
@@ -15,19 +16,41 @@ class TaskDetailView(generic.DetailView):
 
 class TaskCreateView(generic.CreateView):
     model = Task
-    fields = "__all__"
+    form_class = TaskForm
     success_url = reverse_lazy("todo_list:task-list")
 
 
 class TaskUpdateView(generic.UpdateView):
     model = Task
-    fields = "__all__"
+    form_class = TaskForm
     success_url = reverse_lazy("todo_list:task-list")
 
 
 class TaskDeleteView(generic.DeleteView):
     model = Task
     success_url = reverse_lazy("todo_list:task-list")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    success_url = reverse_lazy("todo_list:tag-list")
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_list:tag-list")
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_list:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo_list:tag-list")
+
 
 
 
