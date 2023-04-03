58014809152ff951651a2ad312298bb866e2e998
ostapix@gmail.com <@Bandera345>
2023-04-03 12:50:12 +0300

    Implement sidebar

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,9 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from todo_list.views import TaskList
 
 urlpatterns = [
     path("", TaskList.as_view(), name="task-list"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 app_name = "todo_list"
